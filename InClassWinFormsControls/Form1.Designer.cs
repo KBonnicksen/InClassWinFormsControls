22082e049d73c0bfa84281906b74604dcfe0f4c2
Kelsey Bonnicksen <kelseybon@gmail.com>
2019-05-14 09:51:01 -0700

    Add project files.

---
+++ InClassWinFormsControls/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace InClassWinFormsControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboExpYear = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboExpYear
+            // 
+            this.cboExpYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboExpYear.FormattingEnabled = true;
+            this.cboExpYear.Location = new System.Drawing.Point(84, 72);
+            this.cboExpYear.Name = "cboExpYear";
+            this.cboExpYear.Size = new System.Drawing.Size(156, 21);
+            this.cboExpYear.TabIndex = 0;
+            this.cboExpYear.SelectedIndexChanged += new System.EventHandler(this.cboExpYear_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboExpYear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboExpYear;
+    }
+}
+
---
+++ InClassWinFormsControls/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InClassWinFormsControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateExpirationYears();
+        }
+
+        /// <summary>
+        /// Populates a list of the next 5 years 
+        /// into the expiration year combobox
+        /// </summary>
+        private void PopulateExpirationYears()
+        {
+            cboExpYear.Items.Add(" - - Choose an Exp Year - -");
+
+            int currYear = DateTime.Now.Year;
+            int endYear = currYear + 5;
+            for (int i = currYear; i <= endYear; i++)
+            {
+                cboExpYear.Items.Add(i);
+            }
+
+            cboExpYear.SelectedIndex = 0;
+
+        }
+
+        private void cboExpYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //fired everytime the index is changed
+
+            if(cboExpYear.SelectedIndex == 0)
+            {
+                //Like a break that takes you out of the entire method
+                return;
+            }
+            int chosenYear = (int)cboExpYear.SelectedItem;
+            MessageBox.Show($"You chose {chosenYear}");
+        }
+    }
+}
